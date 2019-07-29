bfb79f29b47a6ba36a1f3faac5ba34fe8d7e7adf
Domenico Martella <domenico.martella@alcacoop.it>
2019-07-29 16:23:41 +0200

    FIX: disabled addCCommas

--- src/util/format.js
+++ src/util/format.js
@@ -31,9 +31,7 @@ export function addCommas(x) {
     if (isNaN(x)) {
         return '-';
     }
-    x = (x + '').split('.');
-    return x[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,')
-            + (x.length > 1 ? ('.' + x[1]) : '');
+    return x
 }
 
 /**
